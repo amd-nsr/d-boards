9a487e1d9f3a665d81c8c32b29ab345398d16cfe
b-e-d-o <36853269+b-e-d-o@users.noreply.github.com>
2021-06-11 01:44:57 +0200

    settings file modifying

--- main/settings/prodduction.py
+++ main/settings/prodduction.py
@@ -7,7 +7,6 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 os.environ['DJANGO_SETTINGS_MODULE'] = 'main.settings.production'
-
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
