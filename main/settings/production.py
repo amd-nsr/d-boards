4fedcf3bab9aa6355908f363a9f94475ae779541
b-e-d-o <36853269+b-e-d-o@users.noreply.github.com>
2021-06-11 16:43:31 +0200

    static variables edit

--- main/settings/production.py
+++ main/settings/production.py
@@ -1,11 +1,13 @@
 from main.settings.base import *
 import os
+from pathlib import Path
 import django_heroku
 import dj_database_url
 from decouple import config
 
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
+#DEBUG = os.environ.get('DEBUG', False)
 #ALLOWED_HOSTS = ['d-boards.herokuapp.com']
 ALLOWED_HOSTS = ['*']
 os.environ['DJANGO_SETTINGS_MODULE'] = 'main.settings.production'
@@ -16,16 +18,17 @@ DATABASES = {
 }
 
 # Static files (CSS, JavaScript, Images)
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-BASE_DIR = os.path.dirname(PROJECT_ROOT)
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     #os.path.join(PROJECT_ROOT, 'static'),
     os.path.join(BASE_DIR, 'static'),
 )
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
