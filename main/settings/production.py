ec3a39cba99a4103cc981083046dc37c053dd1a7
b-e-d-o <36853269+b-e-d-o@users.noreply.github.com>
2021-06-11 13:59:11 +0200

    wsgi whitenoise reconfig

--- main/settings/production.py
+++ main/settings/production.py
@@ -24,6 +24,6 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
     #os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
